d6e1edb7025a7fb3977d71a6ea4d2da8339ae3c3
Steven Winston <gpx1000@gmail.com>
2017-09-03 17:05:12 -0700

    Remove some errant files commit working modifications.

--- app/wrap_add_dir/res/arm64-v8a/wrap.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh-from-zygote
-export ASAN_OPTIONS=start_deactivated=1,malloc_context_size=0
-export ASAN_ACTIVATION_OPTIONS=include_if_exists=/data/local/tmp/asan.options.b
-export LD_PRELOAD=libclang_rt.asan-aarch64-android.so
-su -c setenforcing 0
-$@
-su -c setenforcing 1
